58f22713929016c8ba7fde91050ecb8947dfbf68
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2017-01-19 20:25:25 -0500

    finished up after giving talk

--- ng2-example/scripts/build.sh
+++ ng2-example/scripts/build.sh
@@ -1,1 +1,5 @@
-rimraf dist && webpack --config config/webpack.prod.js --progress --profile --bail
+#!/bin/bash
+
+rm -rf ./dist/*
+cp ./web.config ./dist
+webpack --config config/webpack.prod.js --progress --profile --bail
--- ng2-example/scripts/start.sh
+++ ng2-example/scripts/start.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 webpack-dev-server --inline --progress --port 8080
